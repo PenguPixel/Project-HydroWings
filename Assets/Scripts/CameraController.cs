abc571a77893467796ba9802b7a6be6558ca1e21
CodingMuesli <codingmuesli@gmail.com>
2026-06-11 13:27:44 +0200

    add camera movement on y axis with character

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -5,14 +6,30 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform player;
 
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float smoothTime = 0.2f;
+    [SerializeField] private float maxCamOffsetY = 30;
 
+    private float _currentX;
+    private float _yVelocity = 0.0f;
+    
     void Start()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if (player == null) return;
+
+        _currentX = player.position.x;
+        transform.position = new Vector3(_currentX, player.position.y,transform.position.z);
     }
     
     void LateUpdate()
     {
-        transform.position += Vector3.right * (speed * Time.deltaTime);
+        if (player == null) return;
+
+        _currentX += speed * Time.deltaTime;
+
+        float targetY = player.position.y;
+        float currentY = Mathf.SmoothDamp(transform.position.y, targetY, ref _yVelocity, smoothTime);
+        currentY = Math.Clamp(currentY, -maxCamOffsetY, maxCamOffsetY);
+
+        transform.position = new Vector3(_currentX, currentY, transform.position.z);
     }
 }
