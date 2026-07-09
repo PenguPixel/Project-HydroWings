97567b3e713b5afe33a4402984e9551a862e9ccd
philipp <codingmuesli@gmail.com>
2026-07-09 15:27:42 +0200

    add Explosion Handling

--- Assets/Scripts/KamikazeEnemy.cs
+++ Assets/Scripts/KamikazeEnemy.cs
@@ -1,20 +1,26 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
 using static UnityEngine.Quaternion;
 
 public class KamikazeEnemy : Enemy
 {
         [SerializeField] private float attackSpeedMultiplier = 2f;
         [SerializeField] private float rotationSpeed = 15f;
+        
         private bool _isInRange;
         private Vector3 _baseLookDirection = Vector3.left;
+        private float _explosionDelay = 3f;
+        private float _remainingDelay = 0f;
+        public static UnityEvent<Type, Vector3> OnExplosion;
+        public static UnityEvent<KamikazeEnemy> OnCountdownTrigger;
         private void Update()
         {
                 if (_isDead) return;
                 LifetimeHandling();
                 BasicMovementHandling();
-
         }
 
         private void BasicMovementHandling()
@@ -35,11 +41,26 @@ public class KamikazeEnemy : Enemy
 
         private void OnTriggerStay(Collider other)
         {
+                _remainingDelay = _explosionDelay;
                 if (other)
                 {
                         splineAnimate.enabled = false;
                         _isInRange = true;
                         MoveTowardsTarget(other);
+                        TriggerExplosionTimer();
+                }
+        }
+
+        private void TriggerExplosionTimer()
+        {
+                OnCountdownTrigger?.Invoke(this);
+                if (_remainingDelay > 0)
+                {
+                        _remainingDelay -= Time.deltaTime;
+                }
+                else
+                {
+                        OnExplosion?.Invoke(GetType(), transform.position);
                 }
         }
 
