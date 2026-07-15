ca186a3a2179f7e32f6c2edcc7dc1f9ba583cf01
CodingMuesli <codingmuesli@gmail.com>
2026-07-15 15:24:56 +0200

    upd detonation shader

--- Assets/Scripts/KamikazeEnemy.cs
+++ Assets/Scripts/KamikazeEnemy.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Splines;
 using UnityEngine.UIElements;
 using static UnityEngine.Quaternion;
 
@@ -9,11 +10,14 @@ public class KamikazeEnemy : Enemy
 {
         [SerializeField] private float attackSpeedMultiplier = 2f;
         [SerializeField] private float rotationSpeed = 15f;
+        [SerializeField] private float explosionDelay = 3f;
+        [SerializeField] private Renderer kamikazeRenderer;
         
         private bool _isInRange;
         private Vector3 _baseLookDirection = Vector3.left;
-        private float _explosionDelay = 3f;
-        private float _remainingDelay = 0f;
+        private bool _isExploding = false;
+        private float _explosionTimer = 0f;
+
         public static UnityEvent<Type, Vector3> OnExplosion;
         public static UnityEvent<KamikazeEnemy> OnCountdownTrigger;
         private void Update()
@@ -21,8 +25,17 @@ public class KamikazeEnemy : Enemy
                 if (_isDead) return;
                 LifetimeHandling();
                 BasicMovementHandling();
-        }
 
+                if (_isExploding)
+                {
+                        _explosionTimer += Time.deltaTime;
+                        if (_explosionTimer >= explosionDelay)
+                        {
+                                
+                        }
+                }
+        }
+        
         private void BasicMovementHandling()
         {
                 if (!_isInRange)
@@ -41,27 +54,20 @@ public class KamikazeEnemy : Enemy
 
         private void OnTriggerStay(Collider other)
         {
-                _remainingDelay = _explosionDelay;
                 if (other)
                 {
                         splineAnimate.enabled = false;
                         _isInRange = true;
                         MoveTowardsTarget(other);
-                        TriggerExplosionTimer();
+                        TriggerSelfDestruction(other);
                 }
         }
 
-        private void TriggerExplosionTimer()
+        private void TriggerSelfDestruction(Collider other)
         {
-                OnCountdownTrigger?.Invoke(this);
-                if (_remainingDelay > 0)
-                {
-                        _remainingDelay -= Time.deltaTime;
-                }
-                else
-                {
-                        OnExplosion?.Invoke(GetType(), transform.position);
-                }
+        _explosionTimer = 0f;
+        _isExploding = true;
+        
         }
 
         private void OnTriggerExit(Collider other)
