e6ab83f18777036f31e28a64847ad5b7ae262ec1
CodingMuesli <codingmuesli@gmail.com>
2026-07-16 10:57:19 +0200

    fix RaycastTrigger interaction

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -44,7 +44,7 @@ public class Projectile : MonoBehaviour
         if (!_isEnemy)
         {
             _hasHit = Physics.Raycast(transform.position, transform.forward, out RaycastHit hit,
-                distanceThisFrame + Single.Epsilon, LayerMask.GetMask("Enemy"));
+                distanceThisFrame + Single.Epsilon, LayerMask.GetMask("Enemy"), QueryTriggerInteraction.Ignore);
             if (_hasHit)
             {
                 //Debug.Log($"{this.name} hat ein Ziel getroffen: {hit.transform.name}");
@@ -55,7 +55,7 @@ public class Projectile : MonoBehaviour
         else
         {
             _hasHit = Physics.Raycast(transform.position, Vector3.left, out RaycastHit hit,
-                distanceThisFrame + Single.Epsilon, LayerMask.GetMask("Friendly"));
+                distanceThisFrame + Single.Epsilon, LayerMask.GetMask("Friendly"), QueryTriggerInteraction.Ignore);
             if (_hasHit)
             {
                 //Debug.Log($"{this.name} hat ein Ziel getroffen: {hit.transform.name}");
